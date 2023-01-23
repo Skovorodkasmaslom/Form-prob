eb5a3e49f0af30e16bffb5624b7a678b7911d49a
Skovorodka <mac@Mac-Skovorodka.local>
2023-01-24 00:57:48 +0300

    Index.php

--- Index.php
+++ Index.php
@@ -1,69 +1,88 @@
 <?php
 
-$lastname=$firstname=$patronymic=$email=$pass=$confpass="";
-$lastnameErr=$firstnameErr=$patronymicErr=$emailErr=$passErr=$confpassErr="";
+declare(strict_types=1); //включение строгой типизации
 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
+$lastname = $firstname = $patronymic = $email = $pass = $confpass = "";
 
-    if (empty($_POST["lastname"])){
-        $lastnameErr="Введите фамилию";
-    }else{ 
-        $lastname=test_input($_POST["lastname"]);
-    }
+$errors = [];
 
-    if (empty($_POST["firstname"])){
-        $firstnameErr="Введите имя";
+$success = "";
+$denied = "";
+
+//проверка отправки на сервер данных методом ПОСТ
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    //проверка фамилии
+    if (empty($_POST['lastname'])){
+        $errors['lastname'] = "Введите фамилию";
+    }elseif(!validateUsersNames($_POST['lastname'])){
+        $errors['lastname'] = "Введите корректные данные";
     }else{
-        $firstname=test_input($_POST["firstname"]);
+        $lastname = $_POST['lastname'];
     }
-
-    if (empty($_POST["patronymic"])){
-        $patronymicErr="Введите отчество";
+    //проверка имени
+    if (empty($_POST['firstname'])){
+        $errors['firstname'] = "Введите имя";
+    }elseif(!validateUsersNames($_POST['firstname'])){
+        $errors['firstname'] = "Введите корректные данные";
     }else{
-        $patronymic=test_input($_POST["patronymic"]);
+        $firstname = $_POST['firstname'];
     }
-
-    if (empty($_POST["email"])){
-        $emailErr="Введите E-mail";
+    //проверка отчества
+    if (empty($_POST['patronymic'])){
+        $errors['patronymic'] = "Введите отчество";
+    }elseif(!validateUsersNames($_POST['patronymic'])){
+        $errors['patronymic'] = "Введите корректные данные";
     }else{
-        $email=test_input($_POST["email"]);
+        $patronymic = $_POST['patronymic'];
     }
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $emailErr="Неверный формат E-mail";
-    }else{
-        $email=test_input($_POST["email"]);
-    }
-
-    if (empty($_POST["pass"])){
-        $passErr="Введите пароль";
+    //проверка емайла
+    if (empty($_POST['email'])){
+        $errors['email'] = "Введите E-mail";
+    }elseif(!validateEmail($_POST["email"])){
+        $errors['email'] = "Введите корректный E-mail";
     }else{    
-        $pass=test_input($_POST["pass"]);    
+        $email = $_POST['email'];
+    } 
+    //проверка пароля
+    if (empty($_POST['pass'])){
+        $errors['pass'] = "Введите пароль";
+    }elseif(!validatePass($_POST['pass'])){
+        $errors['pass'] = "Пароль должен быть не менее 8 символов и включать в себя только латинские буквы и цифры";
+    }else{    
+        $pass = $_POST['pass'];    
     }
-
-    if (empty($_POST["confpass"])){
-        $confpassErr="Подтвердите пароль";
+    //проверка совпадения пароля
+    if (empty($_POST['confpass'])){
+        $errors['confpass'] = "Подтвердите пароль";
+    }elseif($_POST["confpass"] !== $_POST["pass"]){
+        $errors['confpass'] = "Пароли не совпадают";
     }else{
-        $confpass=test_input($_POST["confpass"]);
-    }
-
-    if ( $_POST["confpass"]!==$_POST["pass"]){
-        $confpassErr="Пароли не совпадают";
-    }else{
-        $confpass=test_input($_POST["confpass"]);
+        $confpass = $_POST['confpass'];
     }
 }
-
-if(((in_array("", $_POST) == false) and (filter_var($email, FILTER_VALIDATE_EMAIL) )) and ($confpass==$pass)){
-    $formdata=print_r($_POST, true);
-    file_put_contents("Formdata.txt", "$formdata \r\n", FILE_APPEND );
+//запись данных в файл и информирование об успехе
+if(isset($_POST['done']) && empty($errors)){
+    $formdata = print_r($_POST, true);
+    file_put_contents("Formdata.txt", "$formdata \r\n", FILE_APPEND);
+    $success = "Регистрация прошла успешно!";
+}else{
+    $denied = "*Заполните все обязательные поля!";
 }
-
-function test_input($data){
-    $data=trim($data);
-    $data=stripslashes($data);
-    $data=htmlspecialchars($data);
-    return $data;
+//валидация имён
+function validateUsersNames($name){
+    if(preg_match('/^([а-яА-ЯЁёa-zA-Z]+)$/u', $name) && mb_strlen($name) > 1 && mb_strlen($name) < 40){
+        return htmlspecialchars(strip_tags(stripslashes(trim($name))));
+    }
+}
+//валидация емайл
+function validateEmail($email){
+    return $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+//валидация пароля
+function validatePass($pass){
+    if(preg_match('/^([a-zA-Z0-9]+)$/u', $pass) && mb_strlen($pass) > 7 && mb_strlen($pass) < 33){
+        return $pass;
+    }
 }
 
 ?>
@@ -79,32 +98,36 @@ function test_input($data){
     <body>
 
     <h2>Регистрационная форма</h2>
-    <p><span class="error">* обязательные поля </span></p>
+        <div>
+            <? if(isset($success)) { ?>
+                <font color="green"><?=$success?></font><? }else{ ?><font color="red"><?=$denied?></font><br/><br/>
+            <? } ?>
+        </div>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
 
         <label>Фамилия:</label><br/>
         <input type="text" name="lastname" placeholder="Lastname" value="<?php echo $lastname;?>">
-        <span class="error">* <?php echo $lastnameErr;?></span><br/>
+        <span class="error">* <?php echo isset($errors['lastname'])?$errors['lastname']:"";?></span><br/>
 
         <label>Имя:</label><br/>
         <input type="text" name="firstname" placeholder="Firstname" value="<?php echo $firstname;?>">
-        <span class="error">* <?php echo $firstnameErr;?></span><br/>
+        <span class="error">* <?php echo isset($errors['firstname'])?$errors['firstname']:"";?></span><br/>
 
         <label>Отчество:</label><br/>
         <input type="text" name="patronymic" placeholder="Patronymic" value="<?php echo $patronymic;?>">
-        <span class="error">* <?php echo $patronymicErr;?></span><br/>
+        <span class="error">* <?php echo isset($errors['patronymic'])?$errors['patronymic']:"";?></span><br/>
 
         <label>E-mail:</label><br/>
         <input type="email" name="email" placeholder="Email" value="<?php echo $email;?>">
-        <span class="error">* <?php echo $emailErr;?></span><br/>
+        <span class="error">* <?php echo isset($errors['email'])?$errors['email']:"";?></span><br/>
 
         <label>Пароль:</label><br/>
         <input type="password" name="pass" placeholder="Password" value="<?php echo $pass;?>">
-        <span class="error">* <?php echo $passErr;?></span><br/>
+        <span class="error">* <?php echo isset($errors['pass'])?$errors['pass']:"";?></span><br/>
 
         <label>Подтверждение пароля:</label><br/>
         <input type="password" name="confpass" placeholder="Confirm Password"  value="<?php echo $confpass;?>">
-        <span class="error">* <?php echo $confpassErr;?></span><br/>
+        <span class="error">* <?php echo isset($errors['confpass'])?$errors['confpass']:"";?></span><br/>
 
         <br/>
         <input type="submit" name="done" value="Отправить"/><br/>
